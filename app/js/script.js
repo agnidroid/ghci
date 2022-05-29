7a526811443e021f54bcd8b9994ff03c8f15cdc0
ABHISHEK RAJ AGNIHOTRI <60811349+agnidroid@users.noreply.github.com>
2022-05-29 10:53:54 +0530

    Add files via upload

---
+++ app/js/script.js
@@ -0,0 +1,9 @@
+(function(){
+    const navbar = document.getElementById("navbar");
+    const links = navbar.getElementsByClassName('link');
+
+    
+    
+    console.log(navbar);
+    console.log(links);
+})();
