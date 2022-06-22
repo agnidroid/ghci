5e60ce72be12d58e69054cbbaf76b59ea6c825be
ABHISHEK RAJ AGNIHOTRI <60811349+agnidroid@users.noreply.github.com>
2022-06-22 07:18:09 +0530

    Delete app directory

--- app/js/script.js
+++
@@ -1,9 +0,0 @@
-(function(){
-    const navbar = document.getElementById("navbar");
-    const links = navbar.getElementsByClassName('link');
-
-    
-    
-    console.log(navbar);
-    console.log(links);
-})();
